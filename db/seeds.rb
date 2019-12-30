24cb1ec10f623f63da776b0c1ec01c4ff84924e1
ksj <ksj@example.com>
2019-12-30 02:47:13 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 # coding: utf-8
-User.create(name: '太郎', age: 12, birth: 2000/12/12)
-User.create(name: '次郎', age: 10, birth: 2002/12/12)
-User.create(name: '三郎', age: 8, birth: 2004/12/12)
+User.create([
+    {name: '太郎', age: 12, birth: '2000/12/12'},
+    {name: '次郎', age: 10, birth: '2002/12/12'},
+    {name: '三郎', age: 8, birth: '2004/12/12'}
+])
+User.create
+User.create
+User.create
