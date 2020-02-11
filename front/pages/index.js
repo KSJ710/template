c997f881a51a2df582720ef4da379d0af3584a32
sakakibarakeiju <sakakibarakeiju@gmail.com>
2020-02-11 18:57:40 +0900

    resolve conflict

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 export default function Index() {
   return (
     <div>
       <p>Hello Next.js</p>
     </div>
   );
-=======
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  );
->>>>>>> 6c6707f4ae02dfca08de4bf57a36386cb74e98c0
 }
