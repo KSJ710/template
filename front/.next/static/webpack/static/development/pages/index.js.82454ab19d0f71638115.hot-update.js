e3a766fee4880064fe14e9cd8cf74712fbc71886
sakakibarakeiju <sakakibarakeiju@gmail.com>
2020-02-11 18:49:43 +0900

    add next

---
+++ front/.next/static/webpack/static/development/pages/index.js.82454ab19d0f71638115.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "dll-reference dll_ef0ff7c60362f24a921f":
+false
+
+})
+//# sourceMappingURL=index.js.82454ab19d0f71638115.hot-update.js.map
