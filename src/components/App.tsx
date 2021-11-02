aa6d04ba59a1ff4a5ccf8eecbdf8a653b92acc0b
KSJ710 <KSJ710@example.com>
2021-11-02 14:41:57 +0900

    エンドポイントのコンポーネント化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './App.module.scss'
 
 const App = (): JSX.Element => {
   return (
     <div className="container mx-auto w-full h-screen bg-green-600">
       <div className="flex justify-center items-center w-auto h-full">
         <Link href="/template">
-          <a className="btn btn--orange btn--radius">クリック</a>
+          <a className={`${styles.btn_orange} ${styles.btn_radius}`}>クリック</a>
         </Link>
       </div>
     </div>
