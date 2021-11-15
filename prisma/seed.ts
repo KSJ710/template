6af6bd63bb174c01d9a8f5d75fd28a076c294aa9
KSJ710 <KSJ710@example.com>
2021-11-16 07:30:32 +0900

    テーブル追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import colors from './seed_data/colors';
+import headParts from './seed_data/head_parts';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.colors.createMany({ data: colors });
+  await prisma.headParts.createMany({ data: headParts });
 }
 
 main()
