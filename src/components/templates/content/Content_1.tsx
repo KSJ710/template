f072c401c59188c83099335f32bbe10ced52084e
KSJ710 <KSJ710@example.com>
2021-12-11 20:23:27 +0900

    メイン画像を追加

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Image from 'next/image';
-import mainImg from 'public/images/tamplate/content/content_1.jpg';
 import styles from './Content_1.module.scss';
 
 const Content_1 = (): JSX.Element => {
-  return (
-    <div className="w-full h-screen bg-red-500">
-      <div className=""></div>
-      <Image src={mainImg} alt="mainImg" />
-    </div>
-  );
+  return <div className={`${styles.tmp_class} w-screen h-screen`}></div>;
 };
 
 export default Content_1;
