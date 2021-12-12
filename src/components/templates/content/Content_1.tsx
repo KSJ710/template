9107484e8f3e564b1b34b5c08e8a0937b25eca2d
KSJ710 <KSJ710@example.com>
2021-12-12 13:58:33 +0900

    クラス名を変更

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Content_1.module.scss';
 
 const Content_1 = (): JSX.Element => {
-  return <div className={`${styles.tmp_class} w-screen h-screen`}></div>;
+  return <div className={`${styles.base} w-screen h-screen`}></div>;
 };
 
 export default Content_1;
