58f8475b155f915b13b4d374e412c6b892652ccd
KSJ710 <KSJ710@example.com>
2021-12-18 11:48:04 +0900

    スタイルを調整

--- src/components/templates/layout_parts/footers/Foot_1.tsx
+++ src/components/templates/layout_parts/footers/Foot_1.tsx
@@ -4,21 +4,25 @@ import styles from './Foot_1.module.scss';
 
 const Footer_1 = (): JSX.Element => {
   return (
-    <nav className="flex gap-[50px] w-[300px]">
-      <ul className="flex gap-[50px] w-[500px]">
+    <nav className={styles.base}>
+      <ul>
         <li>ページ1</li>
         <li>ページ2</li>
         <li>ページ3</li>
         <li>ページ4</li>
         <li>ページ5</li>
       </ul>
-      <ul className="flex">
+      <ul>
         <li>ページ6</li>
         <li>ページ7</li>
         <li>ページ8</li>
         <li>ページ9</li>
         <li>ページ10</li>
         <li>ページ11</li>
+        <li>ページ12</li>
+        <li>ページ13</li>
+        <li>ページ14</li>
+        <li>ページ15</li>
       </ul>
     </nav>
   );
