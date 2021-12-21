9aab0cbfe7a35468bc013fa61265d4f4981ad3b3
KSJ710 <KSJ710@example.com>
2021-12-21 09:15:15 +0900

    footer css 調整

--- src/components/templates/layout_parts/footers/Foot_1.tsx
+++ src/components/templates/layout_parts/footers/Foot_1.tsx
@@ -1,29 +1,45 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Foot_1.module.scss';
 
 const Footer_1 = (): JSX.Element => {
   return (
     <nav className={styles.base}>
-      <ul>
-        <li>ページ1</li>
-        <li>ページ2</li>
-        <li>ページ3</li>
-        <li>ページ4</li>
-        <li>ページ5</li>
-      </ul>
-      <ul>
-        <li>ページ6</li>
-        <li>ページ7</li>
-        <li>ページ8</li>
-        <li>ページ9</li>
-        <li>ページ10</li>
-        <li>ページ11</li>
-        <li>ページ12</li>
-        <li>ページ13</li>
-        <li>ページ14</li>
-        <li>ページ15</li>
-      </ul>
+      <div className={styles.base_inner}>
+        <div className={styles.menu}>
+          <ul>
+            <li>ページ1</li>
+            <li>ページ2</li>
+            <li>ページ3</li>
+            <li>ページ4</li>
+            <li>ページ5</li>
+          </ul>
+          <ul>
+            <li>ページ6</li>
+            <li>ページ7</li>
+            <li>ページ8</li>
+            <li>ページ9</li>
+            <li>ページ10</li>
+            <li>ページ11</li>
+            <li>ページ12</li>
+            <li>ページ13</li>
+            <li>ページ14</li>
+            <li>ページ15</li>
+          </ul>
+        </div>
+        <div className={styles.sns}>
+          <FontAwesomeIcon icon={['fab', 'instagram-square']} />
+          <FontAwesomeIcon icon={['fab', 'twitter']} />
+          <FontAwesomeIcon icon={['fab', 'youtube']} />
+          <FontAwesomeIcon icon={['fab', 'facebook-square']} />
+        </div>
+        <div className={styles.site_policy}>
+          <p>© 2021 Mysite All Rights Reserved.</p>
+          <p>利用規約</p>
+          <p>プライバシーポリシー</p>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps /*, AppContext */ } from 'next/app';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import 'src/styles/global.scss';
 
-// fasのFontAwesomeIconコンポネントを全体で使用できる
-library.add(fas);
+// fasのFontAwesomeIconコンポネントを全体で使用できる下記参照
+// https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react
+library.add(fab, fas);
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
