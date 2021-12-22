b65ff757e910309844f04cf87795944a0d8ea2c2
KSJ710 <KSJ710@example.com>
2021-12-23 06:20:11 +0900

    微修正

--- src/components/templates/menu/SpMenu.tsx
+++ src/components/templates/menu/SpMenu.tsx
@@ -3,33 +3,33 @@ import Link from 'next/link';
 import styles from './SpMenu.module.scss';
 
 const SpMenu = (props) => {
-  const hundleStopPropagation = (e) => e.stopPropagation();
+  const handleStopPropagation = (e) => e.stopPropagation();
 
   return (
     <nav>
       <ul style={{ display: props.toggleDisplay }} className={styles.menu}>
         <Link href="/">
-          <a onClick={hundleStopPropagation}>
+          <a onClick={handleStopPropagation}>
             <li>ページ1</li>
           </a>
         </Link>
         <Link href="#">
-          <a onClick={hundleStopPropagation}>
+          <a onClick={handleStopPropagation}>
             <li>ページ2</li>
           </a>
         </Link>
         <Link href="#">
-          <a onClick={hundleStopPropagation}>
+          <a onClick={handleStopPropagation}>
             <li>ページ3</li>
           </a>
         </Link>
         <Link href="#">
-          <a onClick={hundleStopPropagation}>
+          <a onClick={handleStopPropagation}>
             <li>ページ4</li>
           </a>
         </Link>
         <Link href="#">
-          <a onClick={hundleStopPropagation}>
+          <a onClick={handleStopPropagation}>
             <li>ログイン</li>
           </a>
         </Link>
