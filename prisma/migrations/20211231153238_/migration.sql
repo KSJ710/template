c6345d18814d69f3202e7f25ec9a6f6e7728b3f1
sakakibara <sakakibara@local.com>
2022-01-01 00:40:40 +0900

    font_famililiesのseedをセット

---
+++ prisma/migrations/20211231153238_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "font_families" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "font_families_pkey" PRIMARY KEY ("id")
+);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import colors from './seed_data/colors';
+import fontFamilies from './seed_data/font_families';
 import headParts from './seed_data/head_parts';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.colors.createMany({ data: colors });
+  await prisma.fontFamilies.createMany({ data: fontFamilies });
   await prisma.headParts.createMany({ data: headParts });
 }
 
