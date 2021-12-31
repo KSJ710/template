736fba4d30699a059f0a43bdaf716b6d2f934307
sakakibara <sakakibara@local.com>
2021-12-31 23:38:34 +0900

    font_familyのseedを作成

---
+++ prisma/seed_data/font_families.ts
@@ -0,0 +1,6 @@
+const fontFamilies = [
+  { name: '"Noto Sans JP", sans-serif' },
+  { name: '"Noto Serif JP", serif' },
+  { name: '"M PLUS Rounded 1c", sans-serif' },
+];
+export default fontFamilies;
