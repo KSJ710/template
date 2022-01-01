5e09007296739e50f663acc53ed0399c615da915
sakakibara <sakakibara@local.com>
2022-01-02 05:04:03 +0900

    headerの高さ依存性を解消

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Content_1.module.scss';
 
 const Content_1 = (): JSX.Element => {
-  return <div className={`${styles.base}`}></div>;
+  // contentの高さをheaderの高さ分引いて設定
+  useEffect(() => {
+    const header_height = document.getElementsByTagName('header')[0].offsetHeight;
+    let content_height = window.screen.height - header_height;
+    /* getElementsByClassNameはHTMLCollectionOf<Element>を返し、
+       Elementはstyleを持たないので型変換 */
+    const contentList = document.getElementsByClassName(
+      '_track_element'
+    ) as HTMLCollectionOf<HTMLElement>;
+    contentList[0].style.height = content_height + 'px';
+  });
+
+  return <div className={`${styles.base} _track_element`}></div>;
 };
 
 export default Content_1;
