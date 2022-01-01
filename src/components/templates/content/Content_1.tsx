fc93a2c236bd2e7808e372bc08460d43a86a2eda
sakakibara <sakakibara@local.com>
2022-01-02 05:15:39 +0900

    クラス名を変更

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -9,12 +9,12 @@ const Content_1 = (): JSX.Element => {
     /* getElementsByClassNameはHTMLCollectionOf<Element>を返し、
        Elementはstyleを持たないので型変換 */
     const contentList = document.getElementsByClassName(
-      '_track_element'
+      '_track_content_1'
     ) as HTMLCollectionOf<HTMLElement>;
     contentList[0].style.height = content_height + 'px';
   });
 
-  return <div className={`${styles.base} _track_element`}></div>;
+  return <div className={`${styles.base} _track_content_1`}></div>;
 };
 
 export default Content_1;
