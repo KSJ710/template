5b2e4d5e0043387840f45784d86c1add6cbede5e
sakakibara <sakakibara@local.com>
2022-01-01 15:08:20 +0900

    修正

--- prisma/seed_data/font_families.ts
+++ prisma/seed_data/font_families.ts
@@ -1,6 +1,6 @@
 const fontFamilies = [
-  { name: '"Noto Sans JP", sans-serif' },
-  { name: '"Noto Serif JP", serif' },
-  { name: '"M PLUS Rounded 1c", sans-serif' },
+  { name: 'Noto Sans JP', style: '"Noto Sans JP", sans-serif' },
+  { name: 'Noto Serif JP', style: '"Noto Serif JP", serif' },
+  { name: 'M PLUS Rounded 1c', style: '"M PLUS Rounded 1c", sans-serif' },
 ];
 export default fontFamilies;
