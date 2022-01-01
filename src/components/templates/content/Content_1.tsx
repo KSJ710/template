9543553e01da40a5eb547e3ffea219b2a5dbe3bc
sakakibara <sakakibara@local.com>
2022-01-02 05:19:10 +0900

    画面サイズの取得プロパティを修正

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -5,7 +5,7 @@ const Content_1 = (): JSX.Element => {
   // contentの高さをheaderの高さ分引いて設定
   useEffect(() => {
     const header_height = document.getElementsByTagName('header')[0].offsetHeight;
-    let content_height = window.screen.height - header_height;
+    let content_height = window.innerHeight - header_height;
     /* getElementsByClassNameはHTMLCollectionOf<Element>を返し、
        Elementはstyleを持たないので型変換 */
     const contentList = document.getElementsByClassName(
