1e25e13a81f0fc6d435467d97633c18de573d1c9
sakakibara <sakakibara@local.com>
2022-01-02 05:23:31 +0900

    型定義を追加

--- src/components/templates/content/Content_1.tsx
+++ src/components/templates/content/Content_1.tsx
@@ -4,14 +4,14 @@ import styles from './Content_1.module.scss';
 const Content_1 = (): JSX.Element => {
   // contentの高さをheaderの高さ分引いて設定
   useEffect(() => {
-    const header_height = document.getElementsByTagName('header')[0].offsetHeight;
-    let content_height = window.innerHeight - header_height;
+    const header_height: number = document.getElementsByTagName('header')[0].offsetHeight;
+    let content_height: string = window.innerHeight - header_height + 'px';
     /* getElementsByClassNameはHTMLCollectionOf<Element>を返し、
        Elementはstyleを持たないので型変換 */
     const contentList = document.getElementsByClassName(
       '_track_content_1'
     ) as HTMLCollectionOf<HTMLElement>;
-    contentList[0].style.height = content_height + 'px';
+    contentList[0].style.height = content_height;
   });
 
   return <div className={`${styles.base} _track_content_1`}></div>;
