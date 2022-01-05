fbf1d1b3a534fe64263b40045f499e9029d0af85
sakakibara <sakakibara@local.com>
2022-01-06 08:27:51 +0900

    footの切り替え実装

--- src/components/templates/edit_tools/ChangeFootPart.tsx
+++ src/components/templates/edit_tools/ChangeFootPart.tsx
@@ -1,46 +1,46 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import { useSetRecoilState } from 'recoil';
-import { tplHeadNumberState } from 'src/states/atoms/tamplate_atoms';
+import { tplFootNumberState } from 'src/states/atoms/tamplate_atoms';
 import useSWR from 'swr';
 import axios from 'axios';
-import styles from './ChangeHeadPart.module.scss';
+import styles from './ChangeFootPart.module.scss';
 
 type Props = {
   display: classDisplay;
   setDisplay: React.Dispatch<React.SetStateAction<classDisplay>>;
 };
 
-const ChangeHeadPart = (props: Props): JSX.Element => {
-  const setHeadNum = useSetRecoilState(tplHeadNumberState);
+const ChangefootPart = (props: Props): JSX.Element => {
+  const setFootNum = useSetRecoilState(tplFootNumberState);
 
   //Head部分のパーツを切り替える
-  const changeHeadPart = (e) => {
+  const changeFootPart = (e) => {
     e.stopPropagation();
-    setHeadNum(e.target.value);
+    setFootNum(e.target.value);
   };
 
   //背景カラーエディタを非表示にする
-  const hiddenHeadPart = () => {
+  const hiddenfootPart = () => {
     props.setDisplay('none');
   };
 
-  const { data, error } = useSWR('/api/head_parts', fetcher);
+  const { data, error } = useSWR('/api/foot_parts', fetcher);
   if (error) {
     return <div>error</div>;
   }
   if (!data) {
     return <div>loading...</div>;
   } else {
-    const headPartList = data.map((headPart: LayoutParts) => (
-      <li key={headPart.id} className={styles.tool_list}>
-        <button value={headPart.id} onClick={changeHeadPart}></button>
-        <div className={styles.label}>{headPart.id}</div>
-        {headPart.name}
+    const footPartList = data.map((footPart: LayoutParts) => (
+      <li key={footPart.id} className={styles.tool_list}>
+        <button value={footPart.id} onClick={changeFootPart}></button>
+        <div className={styles.label}>{footPart.id}</div>
+        {footPart.name}
       </li>
     ));
     return (
-      <div style={{ display: props.display }} className={styles.base} onClick={hiddenHeadPart}>
-        <ul className={styles.tool_bg}>{headPartList}</ul>
+      <div style={{ display: props.display }} className={styles.base} onClick={hiddenfootPart}>
+        <ul className={styles.tool_bg}>{footPartList}</ul>
       </div>
     );
   }
@@ -52,4 +52,4 @@ const fetcher: Fetcher = (url) =>
     return res.data;
   });
 
-export default ChangeHeadPart;
+export default ChangefootPart;
