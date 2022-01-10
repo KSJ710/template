a43b3d991d65a940135a70f7dba0c13d147f7220
KSJ710 <KSJ710@example.com>
2022-01-10 15:05:21 +0900

    ロゴミス修正

--- src/components/templates/layout_parts/svg/Logo_1.tsx
+++ src/components/templates/layout_parts/svg/Logo_1.tsx
@@ -3,9 +3,9 @@ type props = {
   width: string;
 };
 
-const Logo_1 = (props): JSX.Element => {
+const Logo_1 = (props: props): JSX.Element => {
   return (
-    <svg height={props.heigth} width={props.width} viewBox="0 0 24 24" fill="#000000">
+    <svg height={props.height} width={props.width} viewBox="0 0 24 24" fill="#000000">
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path d="M19 4h-4L7.11 16.63 4.5 12 9 4H5L.5 12 5 20h4l7.89-12.63L19.5 12 15 20h4l4.5-8L19 4z" />
     </svg>
