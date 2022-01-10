2717f5c29b4d532786bb05a88037c08dc5d99e47
KSJ710 <KSJ710@example.com>
2022-01-10 22:19:36 +0900

    foot2スマホcssを実装

--- src/components/templates/layout_parts/footers/Foot_2.tsx
+++ src/components/templates/layout_parts/footers/Foot_2.tsx
@@ -9,12 +9,12 @@ const Footer_2 = (): JSX.Element => {
   const hTET: (event) => void = useContext(ToggleEditTools);
   return (
     <nav className={`${styles.base} _track_lay_part_Foot`} onClick={hTET}>
-      <div className={styles.contact}>
-        <p>SUBSCRIBE TO OUR NEWSLETTER</p>
-        <address>Email address</address>
-        <button>メルマガ登録</button>
+      <div className={styles.register}>
+        <h2>SUBSCRIBE TO OUR NEWSLETTER</h2>
+        <input className="w-full" type="text" placeholder="Email address" />
+        <input type="submit" value="メルマガ登録" />
       </div>
-      <div className={styles.sns}>
+      <div className={styles.site_info}>
         <ul>
           <li>CONTACT</li>
           <li>FAQ</li>
@@ -24,29 +24,17 @@ const Footer_2 = (): JSX.Element => {
             </div>
           </li>
         </ul>
-      </div>
-      <div className={styles.site_policy}>
-        <p>利用規約</p>
-        <p>プライバシーポリシー</p>
-        <p>特定商取引法に基づく表記</p>
+        <ul>
+          <li>利用規約</li>
+          <li>プライバシーポリシー</li>
+          <li>特定商取引法に基づく表記</li>
+        </ul>
       </div>
       <div className={styles.logo}>
         <Logo height="72px" width="72px" />
         <p>ロゴロゴ</p>
       </div>
-      <p>© 2021 Mysite All Rights Reserved.</p>
-      <div className={styles.test}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      <p className={styles.copy_right}>© 2021 Mysite All Rights Reserved.</p>
     </nav>
   );
 };
