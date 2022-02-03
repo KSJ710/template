caeadeb62638396a895a858d21a2337468cb8c97
KSJ710 <KSJ710@example.com>
2022-02-03 09:04:52 +0900

    seed追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import colors from './seed_data/colors';
 import fontFamilies from './seed_data/font_families';
 import headParts from './seed_data/head_parts';
 import footParts from './seed_data/foot_parts';
+import axios from 'axios';
 
 const prisma = new PrismaClient();
 
@@ -11,12 +12,35 @@ async function main() {
   await prisma.fontFamilies.createMany({ data: fontFamilies });
   await prisma.headParts.createMany({ data: headParts });
   await prisma.footParts.createMany({ data: footParts });
+
+  const [prefectures, cities] = await axios
+    .get('https://geolonia.github.io/japanese-addresses/api/ja.json')
+    .then((res) => {
+      const addresses = JSON.parse(JSON.stringify(res.data));
+      let prefectures = [];
+      let cities = [];
+
+      let num = 1;
+      // 県を配列に入れる
+      for (const ken in addresses) {
+        prefectures.push({ name: ken });
+        // 市町村を配列に入れる
+        for (const city of addresses[ken]) {
+          cities.push({ prefectureId: num, name: city });
+        }
+        // 次の県へ
+        num++;
+      }
+      return [prefectures, cities];
+    });
+  await prisma.prefectures.createMany({ data: prefectures });
+  await prisma.cities.createMany({ data: cities });
 }
 
 main()
   .catch((e) => {
     throw e;
   })
-  .finally(async () => {
-    await prisma.$disconnect();
+  .finally(() => {
+    prisma.$disconnect();
   });
