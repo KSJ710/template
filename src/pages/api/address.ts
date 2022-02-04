d1487b96ed5cb7ba935964b43ae1758903a3f3e8
KSJ710 <KSJ710@example.com>
2022-02-05 00:18:01 +0900

    save

--- src/pages/api/address.ts
+++ src/pages/api/address.ts
@@ -3,21 +3,9 @@ import prisma from 'lib/prisma';
 import { fetcher } from 'src/helper/common';
 import axios from 'axios';
 
-type Fetcher = (url: string) => Promise<any>;
-
 const address = async (req: NextApiRequest, res: NextApiResponse) => {
-  await fetcher('https://geolonia.github.io/japanese-addresses/api/ja.json').then((data) => {
-    // const obj = JSON.parse(JSON.stringify(data));
-    // for (const b in obj) {
-    //   console.log({name: b});
-    // }
-    const address = JSON.parse(JSON.stringify(data));
-    let address_array = [];
-    for (const ken in address) {
-      address_array.push({ name: ken });
-    }
-    res.status(200).json(address_array);
-  });
+  const data = await prisma.prefectures.findMany({ include: { cities: true } });
+  res.status(200).json(data);
 };
 
 export default address;
--- src/pages/api/sample.ts
+++ src/pages/api/sample.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from 'lib/prisma';
 
-const sample = (req: NextApiRequest, res: NextApiResponse) => {
-  const mem = prisma.users.create({});
-  res.status(200);
+const sample = async (req: NextApiRequest, res: NextApiResponse) => {
+  const member = await prisma.member.create({});
+  res.status(200).json(member);
 };
 
 export default sample;
