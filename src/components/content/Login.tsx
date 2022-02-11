dd3d4afdada066e06a93e198afae47525dbf5bd0
KSJ710 <KSJ710@example.com>
2022-02-11 19:37:21 +0900

    不要ファイルを削除

--- src/components/content/Login.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import styles from './Login.module.scss';
-
-const Login = (): JSX.Element => {
-  return <div>bbbbbbb</div>;
-};
-
-export default Login;
